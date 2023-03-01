e11af5fd0541d46285eaa5189e7531431483eb4c
Rooshaan <mirrooshaanrafi@gmail.com>
2023-03-01 19:24:38 +0500

    Modal component implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import { Table } from './components/shared';
+import { useState } from 'react';
+import { Modal, Table } from './components/shared';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleShowModal = () => setShowModal((prev) => !prev);
+
   return (
-    <div className="h-screen flex justify-center items-center bg-emerald-300">
+    <div className="h-screen flex flex-col justify-center items-center bg-emerald-300">
       <div className="w-1/2">
         <Table />
       </div>
+      <div className="flex items-center h-screen justify-center">
+        <button
+          className="px-4 py-2 bg-gradient-to-tl from-blue-300 to-red-300 rounded-md"
+          onClick={toggleShowModal}
+        >
+          Learn More
+        </button>
+      </div>
+
+      <Modal
+        show={showModal}
+        closeHandler={toggleShowModal}
+        containerClassName="w-2/4 p-5"
+        closeButton={true}
+      >
+        <h1 className="font-semibold text-xl">Title</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </Modal>
     </div>
   );
 }
---
+++ src/components/shared/Modal.jsx
@@ -0,0 +1,48 @@
+const Modal = ({
+  show,
+  closeHandler,
+  containerClassName,
+  children,
+  closeButton = false,
+}) => {
+  return (
+    <>
+      <div
+        className={`absolute top-0 left-0 z-50 flex h-screen w-full flex-col items-center justify-center bg-gray-300/70 py-2 ${
+          !show ? 'hidden' : ''
+        }`}
+        onClick={closeHandler}
+      >
+        <div
+          className={`relative mx-auto w-auto rounded-md bg-white p-3 min-w-[300px] shadow-lg ${containerClassName}`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          {closeButton && (
+            <span
+              className="absolute right-2 top-3 cursor-pointer"
+              onClick={closeHandler}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </span>
+          )}
+
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Modal;
